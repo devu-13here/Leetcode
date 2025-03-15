9a5473cda4ad44b2843123877df8e6fac5802ad7
Devanjali Patel <150452256+devu-13here@users.noreply.github.com>
2025-03-15 14:15:23 +0530

    Add files via upload

---
+++ Basic Maths/PrintDivisor/printdivisor.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printDivisors(int n){
+    vector<int> ls;
+    for(int i =1 ; i*i <=(n) ; i++){
+        if(n%i == 0){
+            ls.push_back(i);
+            if((n/i) != i){
+                ls.push_back(n/i);
+            }
+        }
+    }
+    sort(ls.begin() , ls.end());
+    for(auto it: ls){cout << it <<" ";}
+}
+
+int main()
+{
+    int n;
+    cin>> n;
+    printDivisors(n);
+}
